af80f646d0e18b0fab5251719322af67662a6081
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-12-01 16:41:02 +0530

    Initial Commit

---
+++ DataVisualization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp_Assignment_25
+{
+    public class DataVisualization
+    {
+        private readonly MysharedLibrary.DataVlidator datavalidator;
+        private readonly MysharedLibrary.Logger logger;
+
+        public DataVisualization(MysharedLibrary.DataVlidator dataValidator, MysharedLibrary.Logger logger)
+        {
+            this.datavalidator = dataValidator;
+            this.logger = logger;
+        }
+        public void ProcessUserInput(string userInput)
+        {
+            if (datavalidator.ValidateUserData(userInput))
+            {
+                logger.LogMessage($"User input '{userInput}' is valid.");
+            }
+            else
+            {
+                logger.LogMessage($"Invalid user input: '{userInput}'.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MysharedLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp_Assignment_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MysharedLibrary.DataVlidator dataValidator = new MysharedLibrary.DataVlidator();
+            MysharedLibrary.Logger logger = new MysharedLibrary.Logger();
+
+            UserManagement userManagement = new UserManagement(dataValidator, logger);
+            FileProcessing fielprocessing = new FileProcessing(dataValidator, logger);
+            DataVisualization dataVisualization = new DataVisualization(dataValidator, logger);
+
+
+            Console.Write("Enter username for login: ");
+            string userInput = Console.ReadLine();
+            userManagement.ProcessUserInput(userInput);
+            
+
+             
+            Console.ReadKey();
+        }
+    }
+}
